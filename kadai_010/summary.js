396f2ba3565f9c84dba0db2db47224d1b95da22d
Russianblue41 <158019791+Russianblue41@users.noreply.github.com>
2024-02-21 13:43:19 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#change-color').on('click',function(){
+    $('#target').css('color','skyblue');
+  });
+  $('#change-text').on('click',function(){
+    $('#target').text('ニャー');
+  });
+  $('#fade-out').on('click',function(){
+    $('#target').fadeOut();
+  });
+  $('#fade-in').on('click',function(){
+    $('#target').fadeIn();
+  });
+});
